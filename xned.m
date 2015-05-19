3eb91ebd688e3711b2a78f2a5ad082b12f4cd896
ivan-liljeqvist <ivanli@kth.se>
2015-05-19 16:57:53 +0200

    latest

--- xned.m
+++ xned.m
@@ -1,4 +1,4 @@
-function f=xned(start_angle,draw_graph)
+function [ dist_from_lastx, result_vector] =xned(start_angle,draw_graph)
 
     XY0=[0 19*cos(start_angle) 1.5 19*sin(start_angle)]';
     
@@ -8,8 +8,14 @@ function f=xned(start_angle,draw_graph)
     RESULT_X=[];
     RESULT_Y=[];
     
+    time=0;
+    oldX=0;
+    oldY=1.5;
+    
+    result_vector=[[]];
+    
     while(XY0(3)>0)
-
+            
         f1=varp(XY0);
         f2=varp(XY0+h*f1/2);
         f3=varp(XY0+h*f2/2);
@@ -21,14 +27,30 @@ function f=xned(start_angle,draw_graph)
         RESULT_Y=[RESULT_Y;XY0(3)];
 
         lastX=XY0(1);
-
         
+        x_diff=XY0(1)-oldX;
+        y_diff=XY0(3)-oldY;
+        oldX=XY0(1);
+        oldY=XY0(3);
+        
+        length_of_chunk=sqrt(abs(x_diff)^2+abs(y_diff)^2);
+
+        time=time+length_of_chunk/(sqrt(XY0(4)^2+XY0(2)^2));
+         
+        v=[time XY0(1) XY0(3) XY0(2) XY0(4)];
+        result_vector=[result_vector;v];
+         
     end
     
     if(draw_graph)
         plot(RESULT_X,RESULT_Y);
     end
-    
-f=20-lastX;
 
+disp('TIME WHEN WE HIT THE GROUND');
+time
+
+%result_vector
+dist_from_lastx=20-lastX;
+
+end
  
